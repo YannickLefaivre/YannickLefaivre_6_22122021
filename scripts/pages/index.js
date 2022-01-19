adab1e4a7edf66714b66ef53a50322c9b26adf18
Yannick LEFAIVRE <debeug@outlook.fr>
2022-01-19 16:34:02 -0400

    Affichage dynamique des données sur la page d'accueil (version 1)

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,45 +1,64 @@
-    async function getPhotographers() {
-        // Penser à remplacer par les données récupérées dans le json
-        const photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+async function getPhotographers() {
+    const photographers = fetch("./data/photographers.json")
+                            .then(function(result) {
+                                if(result.ok) {
+                                    return result.json();
+                                }
+                            })
+                            .then(function(photographersDatas) {
+                                return photographersDatas.photographers; // renvoie les données des photographes (nom, ville, pays, etc...)
+                            })
+                            .catch(function(error) {
+                                console.log(`Fetch haven't succeed to retrieve the photographers datas. ${error}.`)
+                            });
+                            
+    return photographers;
+}
+
+async function displayData(photographers) {
+    const photographersThumbnails = document.querySelectorAll(".thumb-photographer");
+
+    /* 
+        Parcours la liste de miniatures de photographe tout en ajoutant leur données 
+        au emplacement prévu à cette effet dans le HTML.
+    */
+    for(i = 0; i < photographers.length && i < photographersThumbnails.length; i++) {
+        photographersThumbnails[i]
+            .querySelector("a[href=\"./pages/photographer.html\"]")
+            .setAttribute("id", `${photographers[i].id}`
+        );
+
+        photographersThumbnails[i]
+            .querySelector(".user")
+            .setAttribute(
+                "src", 
+                `./assets/photographers/photographers-id-photos/${photographers[i].portrait}`
+        );
+
+        photographersThumbnails[i].querySelector(".thumb-photographer__heading").textContent = photographers[i].name;
+
+        photographersThumbnails[i].querySelector(".thumb-photographer-datas__location").textContent = `${photographers[i].city}, ${photographers[i].country}`;
+
+        photographersThumbnails[i].querySelector(".thumb-photographer-datas__tagline").textContent = photographers[i].tagline;
+
+        photographersThumbnails[i].querySelector(".thumb-photographer-datas__price-per-day").textContent = `${photographers[i].price}€/day`;
     }
-
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer-section");
-
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerFactory(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    };
-
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    };
     
-    init();
+/* 
+    Provient du code de base. Laisser-là en commentaire pour l'instant 
+    avant d'être sûr de n'en pas avoir besoin plus tard.
+
+    photographers.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+    }); 
+*/
+};
+
+async function init() {
+    // Récupère les datas des photographes
+    const photographers = await getPhotographers();
+    displayData(photographers);
+};
+
+init();
     
